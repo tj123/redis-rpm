620db8832135018551cfb892fef0421db95c77cb
Your Name <you@example.com>
2018-11-07 13:42:25 +0800

    auto download

--- build.sh
+++ build.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
-usage(){
-  echo "$0 - a script to build nexus-oss rpms"
-  echo "Usage: $0 <spec>"
-}
+#usage(){
+#  echo "$0 - a script to build nexus-oss rpms"
+#  echo "Usage: $0 <spec>"
+#}
 
-if [ -z "$1" ]; then
-  usage
-  exit 1
-fi
+#if [ -z "$1" ]; then
+#  usage
+#  exit 1
+#fi
 
 DIST=$(dirname $(readlink -f $0))/dist
-SPEC=$(readlink -f $1)
+#SPEC=$(readlink -f $1)
+SPEC=redis.spec
+VERSION=$(grep "^Version\:" $SPEC|awk '{print $2}')
+
+if [ ! -f $DIST/SOURCES/redis-$VERSION.tar.gz ]; then
+  [ ! -d $DIST/SOURCES ] && mkdir -p $DIST/SOURCES
+  wget https://codeload.github.com/antirez/redis/tar.gz/$VERSION -O $DIST/SOURCES/redis-$VERSION.tar.gz 
+  if [ "$?" -ne "0" ]; then
+    rm -rf $DIST/SOURCES/redis-$VERSION.tar.gz
+    echo "download redis-$VERSION.tar.gz faild!"
+    exit 1
+  fi
+fi
 
 mkdir -p $DIST/{BUILD,BUILDROOT,RPMS}
 rpmbuild --verbose \
